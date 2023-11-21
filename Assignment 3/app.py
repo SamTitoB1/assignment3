eaff1c98aa9ed3d4d0bd1c2e2480a181a702ffa1
San <bood0017@algonquinlive.com>
2023-11-21 12:31:09 -0500

    Added Files

---
+++ Assignment 3/app.py
@@ -0,0 +1,56 @@
+import requests
+# This script is getting data from CoinDesk API
+
+url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+
+def showUsd():
+
+    response = requests.get(url) # sending request to url
+    jsonResponse = response.json() # saving response as JSON
+    time = jsonResponse["time"]["updated"] # getting time from response
+    code = jsonResponse["bpi"]["USD"]["code"] # getting currency code from response
+    rate = jsonResponse["bpi"]["USD"]["rate"] # getting price in USD from response.
+
+    print(time,code,rate)
+
+def showGbp():
+
+    response = requests.get(url)  # sending request to url
+    jsonResponse = response.json()  # saving response as JSON
+    time = jsonResponse["time"]["updated"]  # getting time from response
+    code = jsonResponse["bpi"]["GBP"]["code"]  # getting currency code from response
+    rate = jsonResponse["bpi"]["GBP"]["rate"]  # getting price in USD from response.
+
+    print(time, code, rate)
+
+def showEuro():
+
+    response = requests.get(url)  # sending request to url
+    jsonResponse = response.json()  # saving response as JSON
+    time = jsonResponse["time"]["updated"]  # getting time from response
+    code = jsonResponse["bpi"]["EUR"]["code"]  # getting currency code from response
+    rate = jsonResponse["bpi"]["EUR"]["rate"]  # getting price in USD from response.
+
+    print(time, code, rate)
+
+while True:
+    try:
+        print("1. Show Bitcoin Price in USD.")
+        print("2. Show Bitcoin Price in GBP.")
+        print("3. Show Bitcoin Price in EUR.")
+        userInput = int(input("Please Enter Your Choice: "))
+        if userInput == 1:
+            showUsd()
+            break
+        elif userInput == 2:
+            showGbp()
+            break
+        elif userInput == 3:
+            showEuro()
+            break
+        else:
+            print("Something Went Wrong, Please Try Again...")
+            continue
+    except:
+        print("Something Went Wrong, Quitting...")
+        break
